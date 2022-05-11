2a16c8df0a02f2c7682e62b0b75fb16e28e746aa
roaco <roaco_91@hotmail.com>
2022-05-10 23:30:43 -0500

    make it better

--- mysql/config.js
+++ mysql/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "mundo",
+  password: "rodri",
   database: "onlineshop",
 });
 
