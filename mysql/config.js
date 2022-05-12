ed043d6dd5b5e30b35683203809ad1e93809483a
roaco <roaco_91@hotmail.com>
2022-05-12 00:39:38 -0500

    Cambios en la configuracion de la BD para desplegar

--- mysql/config.js
+++ mysql/config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "rodri",
-  database: "onlineshop",
+  host: "bz19wlktes4tn01bh54y-mysql.services.clever-cloud.com",
+  user: "u9fdyoucukauvfgx",
+  password: "TOKIQXyNm2iXYFd8HHMR",
+  database: "bz19wlktes4tn01bh54y",
 });
 
 module.exports = mysqlConnection;
