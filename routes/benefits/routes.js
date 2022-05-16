e0a494848f3cda372d84a73f47f3fa4135b18629
roaco <roaco_91@hotmail.com>
2022-05-15 23:28:26 -0500

    Se configura cors

--- routes/benefits/routes.js
+++ routes/benefits/routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
+var cors = require('cors')
 const router = express.Router();
 const benefitController = require("../../controllers/benefits");
 
-router.get("/getAll", benefitController.getAll);
-router.delete("/deleteAll", benefitController.deleteAll);
+var corsOptions = {
+    origin: 'http://localhost:3050',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+router.get("/getAll", cors(corsOptions), benefitController.getAll);
+router.delete("/deleteAll",  benefitController.deleteAll);
 
 // app.get('/download', function (req, res) {
 //   var file = '../static/contratoTrato_ex.doc';
