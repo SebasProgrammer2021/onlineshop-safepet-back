f144ce62a053df5ebac90135e6cf8dc459a88485
Sebastian <jstobona@uqvirtual.edu.co>
2022-05-23 18:05:19 -0500

    pruebas unitarias plan corregidas

--- routes/plan/plan.test.js
+++ routes/plan/plan.test.js
@@ -28,32 +28,28 @@ describe("Se prueban valores puntuales de la tabla plan", () => {
 });
 
 //revisar los errores de valores duplicados
-/*describe("Validamos que si esta tomando los datos de la tabla", () => {
+describe("Validamos que si esta tomando los datos de la tabla", () => {
   test("Comprobamos que los objetos son iguales", async () => {
     return plan.getAll().then((data) => {
       let register = [
-        { idplan: 1, copago: 100000, valor: 215000 },
-        { idplan: 2, copago: 100000, valor: 275000 },
-        { idplan: 3, copago: 95000, valor: 365000 },
-        { idplan: 4, copago: 90000, valor: 900000 },
-        {
-          idplan: 5,
-          copago: 60000,
-          valor: 400000,
-        },
-        { idplan: 6, copago: 60000, valor: 455000 },
-        { idplan: 7, copago: 5000, valor: 425000 },
-        { idplan: 8, copago: 8000, valor: 16000 },
-        { idplan: 9, copago: 8000, valor: 16000 },
-        { idplan: 10, copago: 8000, valor: 16000 },
-        { idplan: 11, copago: 8000, valor: 16000 },
-        { idplan: 13, copago: 8000, valor: 16000 },
-        { idplan: 14, copago: 8000, valor: 16000 },
-        { idplan: 18, copago: 5000, valor: 192000 },
-        { idplan: 31, copago: 5000, valor: 170000 },
+        { idPlan: 1, copago: 100000, valor: 215000 },
+        { idPlan: 2, copago: 100000, valor: 275000 },
+        { idPlan: 3, copago: 95000, valor: 365000 },
+        { idPlan: 4, copago: 90000, valor: 900000 },
+        { idPlan: 5, copago: 60000, valor: 400000 },
+        { idPlan: 6, copago: 60000, valor: 455000 },
+        { idPlan: 7, copago: 5000, valor: 425000 },
+        { idPlan: 8, copago: 8000, valor: 16000 },
+        { idPlan: 9, copago: 8000, valor: 16000 },
+        { idPlan: 10, copago: 8000, valor: 16000 },
+        { idPlan: 11, copago: 8000, valor: 16000 },
+        { idPlan: 13, copago: 8000, valor: 16000 },
+        { idPlan: 14, copago: 8000, valor: 16000 },
+        { idPlan: 18, copago: 5000, valor: 192000 },
+        { idPlan: 31, copago: 5000, valor: 170000 },
         
       ];
       expect(data).toEqual(register);
     });
   });
-});*/
+});
